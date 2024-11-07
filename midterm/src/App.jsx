3118d8039cc64dbf62673e39056166e5fbf39032
Travis Schellenberg <travis.schellenberg@gmail.com>
2024-11-06 23:09:01 -0800

    got the basic pages setup with the homepage diplaying products

--- midterm/src/App.jsx
+++ midterm/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { useRoutes } from 'react-router-dom';
+import './App.css';
+import { API_URL } from './constant';
+import Header from './components/Header/header';
+import HomePage from './pages/Homepage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+  let element = useRoutes(
+    [
+      {
+        path: '/',
+        element: <HomePage />
+      },
+      {
+        path:'/product',
+        element: <ProductDetailPage />
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />
+      }
+
+    ]
   )
+
+  return element;
 }
 
 export default App
